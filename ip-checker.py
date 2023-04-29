e5ea4cc2c0fe14104be8277f02f4d33a544a930a
itismhn <itismhn@yahoo.com>
2023-04-29 20:57:33 +0330

    complete input function

--- ip-checker.py
+++ ip-checker.py
@@ -1,5 +1,20 @@
 import requests
+import ipaddress
 import json
+import sys
+
+def check_input_ip():
+    ip_address=input("Enter a valid IP:")
+    try:
+        ip = ipaddress.ip_address(ip_address)
+        if ip.is_private:
+            print(f"{ip_address} is a private IP address.\n and is only used in internal network environments")
+            sys.exit()
+        else:
+            print(f"{ip_address} is a public IP address.")
+    except ValueError:
+        print(f"{ip_address} is not a valid IP address.")
 
 
-abused_api_key = "<api key>"
+
+check_input_ip()
